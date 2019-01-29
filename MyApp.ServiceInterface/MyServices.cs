7b124ee2594c5cc707883da8e60461c36680f995
wwwlicious <773dcf03@opayq.com>
2019-01-29 10:55:16 +0000

    adding mvc example where menu route is not matched

--- MyApp.ServiceInterface/MyServices.cs
+++ MyApp.ServiceInterface/MyServices.cs
@@ -14,5 +14,26 @@ namespace MyApp.ServiceInterface
         {
             return new HelloResponse { Result = $"Hello, {request.Name}!" };
         }
+
+        public object Any(ViewIndex request) => new PageResult(Request.GetPage("/index"));
+        public object Any(ViewContact request) => new PageResult(Request.GetPage("/contact"));
+        public object Any(ViewAbout request) => new PageResult(Request.GetPage("/about"));
+    }
+
+    [Route("/about")]
+    public class ViewAbout
+    {
+    }
+
+    [Route("/contact")]
+    public class ViewContact
+    {
+    }
+
+    [Route("/index")]
+    [FallbackRoute("/{PathInfo*}", Matches="AcceptsHtml")]
+    public class ViewIndex
+    {
+        public string PathInfo { get; set; }
     }
 }
